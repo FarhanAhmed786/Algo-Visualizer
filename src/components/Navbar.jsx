09b43f3d6922b0287d565aa9633ca8bd3fcc441a
Farhan Ahmed <farhan2004ahmed@gmail.com>
2024-07-01 02:09:04 +0530

    Implement Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import bmsit from "../assets/bmslogo.png"
-function Navbar() {
+import './Navbar.css'
+// import Visualizer from "./Visualizer"
 
+// eslint-disable-next-line react/prop-types
+function Navbar({clickBtn}) {
   return (
    <>
      <div className="h-15 bg-slate-100 justify-between text-black flex p-5 gap-10 text-xl flex-wrap ">
@@ -8,10 +11,10 @@ function Navbar() {
            <a href="https://bmsit.ac.in/"> <img src={bmsit} alt="Hi" className="h-16 w-18 justify-around" /></a>
         </div>    
      <div className="sm:flex-grow-[2] flex space-x-6 flex-wrap justify-center items-center ">
-            <a href="#" className="hover:-translate-y-1 hover:text-red-400 transition delay-100 transform">Selection Sort</a>
-            <a href="#" className="hover:-translate-y-1 hover:text-red-400 transition delay-100">Bubble Sort</a>
-            <a href="#" className="hover:-translate-y-1 hover:text-red-400 transition delay-100">Merge Sort</a>
-            <a href="#" className="hover:-translate-y-1 hover:text-red-400 transition delay-100">Quick Sort</a>
+            <button className="btn-hover" onClick={()=>(clickBtn('selection'))}>Selection Sort</button>
+            <button className="btn-hover" onClick={()=>(clickBtn('bubble'))}>Bubble Sort</button>
+            <button className="btn-hover" onClick={()=>(clickBtn('merge'))}>Merge Sort</button>
+            <button className="btn-hover" onClick={()=>(clickBtn('quick'))}>Quick Sort</button>
         </div>
             
      </div>
