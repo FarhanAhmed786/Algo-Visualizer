5610c301620f229cd093fa43ac78efad20deb8a0
Farhan Ahmed <farhan2004ahmed@gmail.com>
2024-07-08 02:28:06 +0530

    add Bubble sort

---
+++ src/components/Bubble.jsx
@@ -0,0 +1,50 @@
+// import React from 'react'
+
+import { useState } from "react";
+import MyButton from "./MyButton";
+
+function Bubble() {
+  const [tile,setTile]=useState([]);
+  const [sorted,setSorted] = useState(false);
+ 
+ 
+    
+  const resetTile = ()=>{
+    const newArray = Array.from({length:20},()=>(Math.floor(Math.random()*100)+1));
+    setTile(newArray);
+    setSorted(!sorted);
+  }
+  
+  
+  const bubble = async () => {
+    let array = [...tile];
+    let n = array.length;
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (array[j] > array[j + 1]) {
+          [array[j], array[j + 1]] = [array[j + 1], array[j]];
+          setTile([...array]);
+           await new Promise(resolve => setTimeout(resolve,100));// can implement speed of the algo 
+        }
+      }
+    }
+    setSorted(true);
+  };
+  return (
+    <div className="visualizerContainer">
+      <div className="tileContainer h-[450px]">
+        {
+          tile.map((value,index)=>(
+            <div className="pile" key={index} style={{height:`${value*4}px`,width:"3rem"}}>
+             {value}
+            </div>
+          ))
+        }
+      </div>
+      <MyButton resetTile={resetTile} algo={bubble}/>
+    </div>
+      
+  )
+}
+
+export default Bubble   
