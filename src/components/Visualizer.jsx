ef69d64aa6e66b2fe1c6cce0df0a7239318402bd
Farhan Ahmed <farhan2004ahmed@gmail.com>
2024-07-08 02:25:55 +0530

    Component to render on selection

--- src/components/Visualizer.jsx
+++ src/components/Visualizer.jsx
@@ -1,29 +1,27 @@
-// import React from 'react'
-// import Navbar from "./Navbar"
-// import isVisible from "./Navbar"
+
 import Merge from "./Merge";
 import Bubble from "./Bubble";
 import "./Visualizer.css"
+import Quick from "./Quick";
+import Selection from "./Selection";
+
+
 // eslint-disable-next-line react/prop-types
-export default function Visualizer({algorithm}) {
-  if(algorithm === ''){
+function Visualizer({algorithm}) {
+  console.log(algorithm);  
+  if(algorithm=== ''){
     return null;
   }
   
   return (
     <>
-      {algorithm === "merge" && <div><Merge/></div>}
-      {algorithm === "selection" && <div>this is Selection Sort</div>}
-      {algorithm === "bubble" && <div><Bubble/></div>}
-      {algorithm === "quick" && <div>this is quick Sort</div>}
-      
-      <div className="controls mb-2 h-1/2 w-full justify-center items-end flex bg-white-700">
-        <div className="flex space-x-4">
-          <button className="btn btn-hover">Reset Piles</button>
-          <button className="btn btn-hover">Sort Piles</button>
-        </div>
-      </div>
-      
+      {algorithm === "merge" && <div><Merge /></div>}
+      {algorithm === "selection" && <div><Selection/></div>}
+      {algorithm === "bubble" && <div><Bubble /></div>}
+      {algorithm === "quick" && <div><Quick/></div>}
     </>
   )
 }
+
+
+export default Visualizer;
