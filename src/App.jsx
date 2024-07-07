c7a430ae7dee5ea4f4ec6c270c85b1a0d272b779
Farhan Ahmed <farhan2004ahmed@gmail.com>
2024-07-08 02:23:20 +0530

    final app structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import {useState} from 'react';
 import './App.css'
 import Navbar from './components/Navbar'
 import Visualizer from './components/Visualizer'
-// import Test from './components/Test'
 
 function App() {
   const [algorithm,setAlogrithm] = useState('');
@@ -10,14 +9,10 @@ function App() {
   const clickBtn = (algorithm)=>{
       setAlogrithm(algorithm);
   };
-
   return (
     <>
-      {/* <div className="text-cyan-400 text-xxl bg-red-400">Hello Farhan</div>       */}
       <Navbar clickBtn={clickBtn}/>
       <Visualizer algorithm={algorithm}/>
-      {/* <Test/> */}
-      <div className="text-red-400 text-xxl bg-blue-500">Test div Farhan</div>      
     </>
   )
 }
